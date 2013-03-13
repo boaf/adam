527b9016ad8e26f465b6bdc2960dce0f3fea1c60
Gibson Starkweather <gibson@gibson.fm>
2013-03-13 11:29:32 -0700

    Being cute. Reorganized files, added TODO stuff and requirements to README

--- adam.py
+++ adam.py
@@ -15,7 +15,7 @@ def skills():
 
 @app.route('/humans.txt')
 def humans_txt():
-    response = app.make_response(open('humans.txt').read())
+    response = app.make_response(open('static/humans.txt').read())
     response.content_type = "text/plain"
     return response
 
