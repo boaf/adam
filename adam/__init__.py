1ef02993096a8c74525725cc80b01635367994ec
Gibson Starkweather <gibson@gibson.fm>
2013-03-13 20:29:07 -0700

    Changing to MySQL

--- adam/__init__.py
+++ adam/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {'DB': 'adam'}
 app.config['SECRET_KEY'] = "A9D'y4WI@yf{d2/1=;pw=END[odzH62oC8iO+0b))1tD63HT2<tgJ{>e'2)?l@*"
 
-db = MongoEngine(app)
+db = MySQL()
+db.init_app(app)
