cdfa0524205d8098be1f9c668750eaead8cc3ec0
Thomas Mashek <thomas@thescoundrels.net>
2013-03-19 21:26:43 -0700

    fixing my icky

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from adam import app
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-#    app.run(debug=True)
-=======
     app.run(debug=True)
->>>>>>> fc816e5807125c278f6301b6e1bc7fa5011d6714
-    app.run(host='0.0.0.0')
