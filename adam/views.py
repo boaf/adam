56204a4429c05592f9dcf94fa0450ce2dd718fc7
Thomas Mashek <thomas@thescoundrels.net>
2013-03-19 21:13:09 -0700

    added todo

--- adam/views.py
+++ adam/views.py
@@ -111,3 +111,7 @@ def humans_txt():
     response = app.make_response(open(os.path.dirname(os.path.realpath(__file__)) + '/../AUTHORS').read())
     response.content_type = "text/plain"
     return response
+
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
