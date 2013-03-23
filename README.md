f5412471d7e9b1d2d5f1a05d0bb13346ce98c65c
Gibson Starkweather <gibson@gibson.fm>
2013-03-22 22:35:48 -0700

    Stuff; moar tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Adam
+
 Adam is a soon-to-be feature-rich utility for monitoring and analyzing many parts of the Massively Multiplayer Online game EVE Online.
 
 [![Build Status](https://travis-ci.org/scoundrels/adam.png?branch=master)](https://travis-ci.org/scoundrels/adam)
--- tests.py
+++ tests.py
@@ -1,19 +1,39 @@
 import os
-import adam
 import unittest
 import tempfile
+
+from adam import app, oid
+from adam.cache import Cache
+from adam.db import db_session, init_db
+from adam.models import User
 
 class AdamTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, adam.app.config['DATABASE'] = tempfile.mkstemp()
-        self.app = adam.app.test_client()
-        adam.db.init_db()
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = app.test_client()
+        init_db()
+
+        db_session.add(User('Test User', 'test@example.com', 'testopenid'))
+        db_session.commit()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(adam.app.config['DATABASE'])
+        os.unlink(app.config['DATABASE'])
 
+    #
+
+    def test_request_login(self):
+        rv = self.app.get('/login')
+        assert '<h3>Sign in</h3>' in rv.data
+
+    def test_after_login(self):
+        user = User.query.filter_by(openid='testopenid').first()
+        assert user is not None
+
+    def test_logout(self):
+        rv = self.app.get('/logout', follow_redirects=True)
+        assert 'You have been signed out' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
